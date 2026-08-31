4242c9755f0acdb39cee789a8c8b3bba613872e7
Riya Verma <ryaverma28@gmail.com>
2026-08-30 23:23:19 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+# In Python a single variable's type can change throughout the code. This is called dynamic typing. For example, the following code will run without any errors:
+# variable = 10
+# variable = "Hello"
+# variable = [1, 2, 3]
+
+variable = 10
+print(type(variable))
+
+variable = 21.5
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "Hello"
+print(type(variable))
+
+variable = [3,6,9]
+print(type(variable))
