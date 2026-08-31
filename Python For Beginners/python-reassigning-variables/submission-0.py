d0b4c7a44bd483de6d38b9f2f25f0ac1e22dc4cc
Riya Verma <ryaverma28@gmail.com>
2026-08-30 23:11:36 -0700

    Add: python-reassigning-variables - submission-0

---
+++ Python For Beginners/python-reassigning-variables/submission-0.py
@@ -0,0 +1,13 @@
+# Two variables can not have the same name. This is because variables can change their value. This is called reassigning a variable. message was first assigned the value "The first message", but then it was reassigned the value "The second message".
+
+message = "The first message"
+
+print(message)
+
+message = "The second message"
+
+print(message)
+
+message = "The third message"
+
+print(message)
