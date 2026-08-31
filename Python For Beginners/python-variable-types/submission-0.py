0dfed792f9f5f35fe34c601464191a43d374cfe3
Riya Verma <ryaverma28@gmail.com>
2026-08-30 23:21:10 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+# We can print the type of a variable or value using the type() function in Python. Ex. print(type(10)), this will output <class 'int'>
+
+integer_type = 10
+float_type = 21.5
+bool_type = False
+string_type = "Hello"
+list_type = [3,6,9]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
