3a341ee4b8576d66dd0c05be1a26917809644c2f
Riya Verma <ryaverma28@gmail.com>
2026-08-30 23:18:28 -0700

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,16 @@
+# Python allows you to assign multiple variables in a single line. Just separate the variables with a comma, and the right-hand side values with a comma as well.
+
+msg1, msg2 = "World", "Hello"
+msg3, msg4, msg5 = "Name", "Is", "My"
+# Don't change the code above this line
+
+msg1, msg2 = msg2, msg1
+msg3, msg4, msg5 = msg5, msg3, msg4
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
