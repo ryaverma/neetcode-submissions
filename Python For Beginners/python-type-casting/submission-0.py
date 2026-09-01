19ed6b198c7c7a8311493be1cf71847d7b0b1bde
Riya Verma <ryaverma28@gmail.com>
2026-09-01 11:14:55 -0700

    Add: python-type-casting - submission-0

---
+++ Python For Beginners/python-type-casting/submission-0.py
@@ -0,0 +1,12 @@
+# A variable in Python can be converted to a different type using type casting.
+
+pi = 3.141592653589793
+square_root_8 = 2.8284271247461903
+# Do not modify above this line
+
+pi = int(pi)
+square_root_8 = int(square_root_8)
+
+# Do not modify below this line
+print(pi)
+print(square_root_8)
