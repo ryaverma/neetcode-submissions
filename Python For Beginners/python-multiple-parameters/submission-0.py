2315189641dcadeb0fbadd389833a424e4cacd3a
Riya Verma <ryaverma28@gmail.com>
2026-09-02 14:17:54 -0700

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,13 @@
+# Functions can be defined to accept more than one parameter. The parameters are separated by commas in the function definition. When calling the function, the arguments are also separated by commas.
+
+def two_sum(n1, n2):
+    print(n1+n2)
+
+def three_sum(n1, n2, n3):
+    print(n1+n2+n3)
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
