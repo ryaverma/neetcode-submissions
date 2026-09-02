21416e01af032b6a52de4d704461bb3242894fd0
Riya Verma <ryaverma28@gmail.com>
2026-09-01 23:42:51 -0700

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,9 @@
+# Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, division, and more. The result of division is always a float in Python. For the other arithmetic operators, the result will be an integer if both operands are integers. If one of the operands is a float, the result will be a float. The order of operations in Python is the same as in mathematics (PEMDAS)
+
+a, b, c = 2, 2, 0.5
+sumdig = a+b+c
+print(sumdig)
+print(0-sumdig)
+muldig = a*b*c
+print(muldig)
+print(sumdig/muldig)
