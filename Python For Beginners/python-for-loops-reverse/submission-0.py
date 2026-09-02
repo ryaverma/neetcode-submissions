0b57ea9fb080caa728b749b138afa3c78dfb2c0a
Riya Verma <ryaverma28@gmail.com>
2026-09-02 16:38:49 -0700

    Add: python-for-loops-reverse - submission-0

---
+++ Python For Beginners/python-for-loops-reverse/submission-0.py
@@ -0,0 +1,4 @@
+# There is a shortcut in Python for iterating through a sequence in reverse order. You can use the function reversed() to reverse a sequence. Example, for i in reversed(range(10)): will iterate from 9 to 0
+
+for i in range(20,9,-1):
+    print(i)
