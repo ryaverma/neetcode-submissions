d4f82f2f43f01c87feb86d8887710de96f4e1470
Riya Verma <ryaverma28@gmail.com>
2026-09-02 00:21:13 -0700

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,12 @@
+# Floor Division //, Modulus %, Exponentiation **
+
+a, b, c = 2, 8, 5
+prodab_floorc = (a*b)//c
+prodab_modc = (a*b)%c
+powerab = a**b
+powerbc = b**c
+
+print(prodab_floorc)
+print(prodab_modc)
+print(powerab)
+print(powerbc)
