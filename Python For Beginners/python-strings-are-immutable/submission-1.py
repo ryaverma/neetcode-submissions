d42d53a2f82c705eabc1b7299e7efab5971a4b3d
Riya Verma <ryaverma28@gmail.com>
2026-09-02 17:23:52 -0700

    Add: python-strings-are-immutable - submission-1

---
+++ Python For Beginners/python-strings-are-immutable/submission-1.py
@@ -0,0 +1,11 @@
+# It's important to know that whenever you slice a string, you are not modifying the underlying string. Instead, you are creating a new string with the sliced characters. This is because strings are immutable in Python, which means they cannot be changed after they are created.
+
+def remove_fourth_character(word: str) -> str:
+    word1 = word[:3]
+    word2 = word[4:]
+    return word1+word2
+
+
+# do not modify below this line
+print(remove_fourth_character("NeetCode"))
+print(remove_fourth_character("Hello"))
