79792123d94c5f2243c4e25ab596f2253ecfe5d7
Riya Verma <ryaverma28@gmail.com>
2026-09-02 20:13:34 -0700

    Add: python-intro-to-lists - submission-0

---
+++ Python For Beginners/python-intro-to-lists/submission-0.py
@@ -0,0 +1,7 @@
+# In Python a list is a collection of items that are stored in a specific order. Lists are mutable, meaning we can change the values of the elements in the list. We can even mix and match different types of elements in a list. This is generally not recommended, but it is possible.
+
+my_list = [1, 7, 5, 4, 3, 2]
+print(my_list[1])
+print(my_list[2])
+print(my_list[0])
+print(len(my_list))
