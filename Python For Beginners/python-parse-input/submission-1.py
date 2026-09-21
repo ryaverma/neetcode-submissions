527652c42d51725f64b221b8fca175dded598327
Riya Verma <ryaverma28@gmail.com>
2026-09-20 21:46:37 -0700

    Add: python-parse-input - submission-1

---
+++ Python For Beginners/python-parse-input/submission-1.py
@@ -0,0 +1,12 @@
+# Sometimes we have to do more than just convert the input to a single type. We might need to restructure the input in some way. This is called parsing the input.
+
+from typing import List
+
+def read_integers() -> List[int]:
+    read = input()
+    return read.split(',')
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
