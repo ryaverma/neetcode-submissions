85429b4e40a009bf8b248bcc7b886cc130801cec
Riya Verma <ryaverma28@gmail.com>
2026-09-20 21:23:48 -0700

    Add: python-read-input - submission-0

---
+++ Python For Beginners/python-read-input/submission-0.py
@@ -0,0 +1,8 @@
+# In Python, the input() function is used to read input to the program while the program is running.
+
+def read_and_print_input() -> None:
+    pinput = input("Reading one line of text into a string: ")
+    print(pinput)
+
+read_and_print_input()
+read_and_print_input()
