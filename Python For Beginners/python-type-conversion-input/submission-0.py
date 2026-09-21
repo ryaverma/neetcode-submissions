00627279c83dd74233a9f973adf72e598119170e
Riya Verma <ryaverma28@gmail.com>
2026-09-20 21:26:08 -0700

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    age = int(input())
+    print(age)
+
+
+def read_float() -> float:
+    marks = float(input())
+    return marks
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
